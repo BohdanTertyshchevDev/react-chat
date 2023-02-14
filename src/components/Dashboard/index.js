b2f1ce6d9221df34f2e2ae5a180702f8ceec7110
Bohdan Tertyshchev <bobterya@gmail.com>
2023-02-14 10:31:59 +0200

    Chat almost make

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,69 @@
+import React, {useState, useReducer, useEffect} from 'react';
+import DialogList from '../DialogList';
+import Chat from '../Chat';
+import MessageArea from '../MessageArea';
+import styles from "./Dashboard.module.css";
+import { UserContext } from '../../contexts';
+import { getData } from '../../API';
+import { reducer } from '../../reducers';
+import CONSTANTS from '../../constants';
+const { ACTIONS } = CONSTANTS;
+
+
+const Dashboard = () => {
+    const [user, setUser] = useState({
+        id: 1,
+        username: "Bohdan Tertyshchev",
+        imageSrc: 'https://robohash.org/Bohdan'
+    });
+
+    const [state, dispatch] = useReducer(reducer, {
+        messages: []
+    })
+
+    useEffect(() => {
+        getData()
+        .then((data) => {
+            dispatch(
+                {
+                    type: ACTIONS.DATA_LOAD_SUCCESS,
+                    data
+                }
+            )
+        })
+        .catch((err) => {
+            dispatch(
+                {
+                    type: ACTIONS.DATA_LOAD_ERROR,
+                    err
+                }
+            )
+        })
+    }, [])
+
+    const createMessage = (text) => {
+        const newMessage = {
+            body: text,
+            id: state.messages.length + 1,
+            user
+        };
+        dispatch({
+            type: ACTIONS.ADD_NEW_MESSAGE,
+            data: newMessage
+        })
+    }
+
+    return (
+        <UserContext.Provider value={user}>
+             <main className={styles.container}>
+                <DialogList/>
+                <section className={styles.wrapper}>
+                <Chat dialog={state.messages}/>
+                <MessageArea submitMessage={createMessage}/>
+                </section>
+            </main>
+        </UserContext.Provider>
+    );
+}
+
+export default Dashboard;
